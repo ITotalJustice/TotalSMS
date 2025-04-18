ITotalJustice/TotalSMS

commit 70dd13b1e1235907cd819c0a22d033dfc01ad4cf

remove all third_party code, pull any libraries using cmake.

src/mgb/ifile/zip/zip.c
@@ -2,6 +2,9 @@
 #include "../../mgb.h"
 #include "../../util.h"
 #include <stdio.h>
+#include <string.h>
+#include <minizip/zip.h>
+#include <minizip/unzip.h>
 
 enum LoadFileType
 {
@@ -16,19 +19,116 @@ typedef struct
     enum LoadFileType type;
 } config_t;
 
-#if 0
-IFile_t* izip_open(const char* path, enum IFileMode mode, int flags)
+union Data
 {
-    return NULL;
-}
-IFile_t* izip_open_mem(void* data, size_t size, enum IFileMode mode, int flags)
+    void* w;
+    const void* r;
+};
+
+typedef struct MzMem
 {
-    return NULL;
+    union Data buf;
+    size_t size;
+    size_t offset;
+    bool read_only;
+} MzMem;
+
+static long minizip_tell_file_func(void* opaque, void* stream)
+{
+    struct MzMem* mem = opaque;
+    return mem->offset;
 }
-#else
-#include <zip.h>
-#include <unzip.h>
-#include <ioapi_mem.h>
+
+static long minizip_seek_file_func(void* opaque, void* stream, unsigned long offset, int origin)
+{
+    struct MzMem* mem = opaque;
+    size_t new_offset = 0;
+
+    switch (origin)
+    {
+        case ZLIB_FILEFUNC_SEEK_SET: new_offset = offset; break;
+        case ZLIB_FILEFUNC_SEEK_CUR: new_offset = mem->offset + offset; break;
+        case ZLIB_FILEFUNC_SEEK_END: new_offset = (mem->size - 1) + offset; break;
+        default: return -1;
+    }
+
+    if (new_offset > mem->size)
+    {
+        return -1;
+    }
+
+    mem->offset = new_offset;
+
+    return 0;
+}
+
+static void* minizip_open_file_func(void* opaque, const char* filename, int mode)
+{
+    return opaque;
+}
+
+static unsigned long minizip_read_file_func(void* opaque, void* stream, void* buf, unsigned long size)
+{
+    struct MzMem* mem = opaque;
+
+    if (mem->size <= mem->offset + size)
+    {
+        size = mem->size - mem->offset;
+    }
+
+    memcpy(buf, (const uint8_t*)mem->buf.r + mem->offset, size);
+    mem->offset += size;
+
+    return size;
+}
+
+static unsigned long minizip_write_file_func(void* opaque, void* stream, const void* buf, unsigned long size)
+{
+    struct MzMem* mem = opaque;
+
+    if (mem->read_only)
+    {
+        return 0;
+    }
+
+    if (mem->size <= mem->offset + size)
+    {
+        mem->buf.w = realloc(mem->buf.w, mem->offset + size);
+        if (mem->buf.w == NULL)
+        {
+            return 0;
+        }
+
+        mem->size = mem->offset + size;
+    }
+
+    memcpy((uint8_t*)mem->buf.w + mem->offset, buf, size);
+    mem->offset += size;
+
+    return size;
+}
+
+static int minizip_close_file_func(void* opaque, void* stream)
+{
+    return 0;
+}
+
+static int minizip_testerror_file_func(void* opaque, void* stream)
+{
+    return 0;
+}
+
+static const zlib_filefunc_def zlib_filefunc = {
+    .zopen_file = minizip_open_file_func,
+    .zread_file = minizip_read_file_func,
+    .zwrite_file = minizip_write_file_func,
+    .ztell_file = minizip_tell_file_func,
+    .zseek_file = minizip_seek_file_func,
+    .zclose_file = minizip_close_file_func,
+    .zerror_file = minizip_testerror_file_func,
+    .opaque = NULL,
+};
+
 #ifdef WIN32
 #include <io.h>
 #else
@@ -135,7 +235,7 @@ typedef struct {
         unzFile u;
         zipFile z;
     } file;
-    ourmemory_t* ourmem;
+    MzMem* mzmem;
     enum IFileMode mode;
 } ctx_t;
 
@@ -189,15 +289,9 @@ static void iclose(void* _private) {
                 break;
         }
 
-        if (ctx->ourmem) {
-            // do NOT free this, unless we own the data
-            // that we passed.
-            if (ctx->ourmem->grow) {
-                free(ctx->ourmem->data.w);
-                ctx->ourmem->data.w = NULL;
-            }
-            free(ctx->ourmem);
-            ctx->ourmem = NULL;
+        if (ctx->mzmem) {
+            free(ctx->mzmem);
+            ctx->mzmem = NULL;
         }
 
         free(ctx);
@@ -237,7 +331,7 @@ static bool iseek(void* _private, long offset, int whence) {
         return false;
     }
 
-    return UNZ_OK == unzSeek(ctx->file.u, (uint32_t)offset, whence);
+    return UNZ_OK == unzSetOffset(ctx->file.u, (uint32_t)offset);
 }
 
 static size_t itell(void* _private) {
@@ -248,7 +342,7 @@ static size_t itell(void* _private) {
     }
 
     // returns uncompressed offset
-    const int32_t r = unzTell(ctx->file.u);
+    const off_t r = unztell(ctx->file.u);
     return r < 0 ? 0 : r;
 }
 
@@ -316,7 +410,7 @@ static IFile_t* internal_open_read(const config_t* config, int flags) {
 
     const ctx_t _ctx = {
         .file.u = file,
-        .ourmem = NULL, // unused!
+        .mzmem = NULL, // unused!
         .mode = IFileMode_READ
     };
 
@@ -399,7 +493,7 @@ static IFile_t* open_write(const char* path, int flags) {
 
     const ctx_t _ctx = {
         .file.z = file,
-        .ourmem = NULL, // unused!
+        .mzmem = NULL, // unused!
         .mode = IFileMode_WRITE
     };
 
@@ -473,18 +567,13 @@ IFile_t* izip_open_fd(int fd, bool own, enum IFileMode mode, int flags) {
     return NULL;
 }
 
-union Data
-{
-    void* w;
-    const void* r;
-};
-
 static IFile_t* open_read_mem(union Data data, size_t size, enum IFileMode mode, int flags) {
     IFile_t* ifile = NULL;
     ctx_t* ctx = NULL;
     unzFile file = NULL;
-    ourmemory_t* ourmemory = NULL;
-    zlib_filefunc_def filefunc32 = {0};
+    MzMem* mzmem = NULL;
+    zlib_filefunc_def filefunc32 = zlib_filefunc;
+    filefunc32.opaque =
 
     ifile = (IFile_t*)malloc(sizeof(IFile_t));
     if (!ifile) {
@@ -496,24 +585,15 @@ static IFile_t* open_read_mem(union Data data, size_t size, enum IFileMode mode,
         goto fail;
     }
 
-    ourmemory = (ourmemory_t*)malloc(sizeof(ourmemory_t));
-    if (!ourmemory) {
+    mzmem = (MzMem*)malloc(sizeof(*mzmem));
+    if (!mzmem) {
         goto fail;
     }
 
-    switch (mode) {
-        case IFileMode_READ:
-            ourmemory->size = size;
-            ourmemory->data.r = (const char*)data.r;
-            fill_memory_filefunc_const(&filefunc32, ourmemory);
-            break;
-
-        case IFileMode_WRITE:
-            ourmemory->size = size;
-            ourmemory->data.w = (char*)data.w;
-            fill_memory_filefunc(&filefunc32, ourmemory);
-            break;
-    }
+    mzmem->buf = data;
+    mzmem->size = size;
+    mzmem->offset = 0;
+    mzmem->read_only = mode == IFileMode_READ;
 
     file = unzOpen2("__notused__", &filefunc32);
     if (!file) {
@@ -522,7 +602,7 @@ static IFile_t* open_read_mem(union Data data, size_t size, enum IFileMode mode,
 
     const ctx_t _ctx = {
         .file.u = file,
-        .ourmem = ourmemory,
+        .mzmem = mzmem,
         .mode = IFileMode_READ
     };
 
@@ -550,22 +630,18 @@ static IFile_t* open_read_mem(union Data data, size_t size, enum IFileMode mode,
 fail:
     if (ifile) {
         free(ifile);
-        ifile = NULL;
     }
 
     if (ctx) {
         free(ctx);
-        ctx = NULL;
     }
 
-    if (ourmemory) {
-        free(ourmemory);
-        ourmemory = NULL;
+    if (mzmem) {
+        free(mzmem);
     }
 
     if (file) {
         unzClose(file);
-        file = NULL;
     }
 
     return NULL;
@@ -600,4 +676,3 @@ IFile_t* izip_open_mem_const(const void* data, size_t size, enum IFileMode mode,
 
     return NULL;
 }
-#endif

src/CMakeLists.txt
@@ -150,18 +150,6 @@ macro(add_static_lib package subdir)
     add_subdirectory(${CMAKE_SOURCE_DIR}/src/third-party/${subdir} ${CMAKE_BINARY_DIR}/src/third-party/${subdir})
 endmacro()
 
-# helper macro that works exactly like find_package()
-# except that it'll fallback to the static lib if the shared
-# lib is not found!
-macro(find_shared_lib package subdir)
-    find_package(${package} QUIET)
-    if (${package}_FOUND)
-        message(STATUS "using shared ${package}")
-    elseif (NOT ${package}_CHECKED)
-        add_static_lib(${package} ${subdir})
-    endif()
-endmacro()
-
 set(USE_MGB OFF)
 
 # this works for now, but will need to be more fancy

src/mgb/CMakeLists.txt
@@ -1,14 +1,5 @@
 cmake_minimum_required(VERSION 3.13.4)
 
-# include(FetchContent)
-
-# FetchContent_Declare(rewind
-#     GIT_REPOSITORY https://github.com/ITotalJustice/librewind.git
-#     GIT_TAG        v1.2.0
-# )
-
-# FetchContent_MakeAvailable(rewind)
-
 include(FetchContent)
 
 FetchContent_Declare(patch
@@ -22,8 +13,6 @@ add_library(mgb
     mgb.c
     romloader.c
     util.c
-    # rewind.c
-    compressors.c
     directory.c
     png/png.c
 
@@ -35,28 +24,102 @@ add_library(mgb
 
 target_include_directories(mgb INTERFACE ${CMAKE_CURRENT_SOURCE_DIR})
 
-option(FORCE_STATIC_LIBS OFF)
-option(STATIC_MINIZIP OFF)
-option(STATIC_ZLIB OFF)
-
-if (VITA)
-    set(STATIC_MINIZIP ON)
-endif()
-
-if (FORCE_STATIC_LIBS)
-    set(STATIC_MINIZIP ON)
-    set(STATIC_ZLIB ON)
-endif()
-
-add_static_lib(minizip minizip)
-find_shared_lib(ZLIB zlib)
-
 target_add_common_cflags(mgb PRIVATE)
+
+set(ZLIB_FOUND FALSE)
+set(MINIZIP_FOUND FALSE)
+
+find_package(ZLIB QUIET)
+
+if (NOT ZLIB_FOUND OR NOT DEFINED ZLIB_VERSION)
+    set(ZLIB_VERSION 1.3.1)
+
+    FetchContent_Declare(zlib
+        GIT_REPOSITORY https://github.com/madler/zlib.git
+        GIT_TAG        v${ZLIB_VERSION}
+        GIT_PROGRESS   TRUE
+        SOURCE_SUBDIR NONE
+    )
+
+    FetchContent_MakeAvailable(zlib)
+
+    add_library(zlib
+        ${zlib_SOURCE_DIR}/adler32.c
+        ${zlib_SOURCE_DIR}/compress.c
+        ${zlib_SOURCE_DIR}/crc32.c
+        ${zlib_SOURCE_DIR}/deflate.c
+        # ${zlib_SOURCE_DIR}/gzclose.c
+        # ${zlib_SOURCE_DIR}/gzlib.c
+        # ${zlib_SOURCE_DIR}/gzread.c
+        # ${zlib_SOURCE_DIR}/gzwrite.c
+        ${zlib_SOURCE_DIR}/infback.c
+        ${zlib_SOURCE_DIR}/inffast.c
+        ${zlib_SOURCE_DIR}/inflate.c
+        ${zlib_SOURCE_DIR}/inftrees.c
+        ${zlib_SOURCE_DIR}/trees.c
+        ${zlib_SOURCE_DIR}/uncompr.c
+        ${zlib_SOURCE_DIR}/zutil.c
+    )
+
+    add_library(ZLIB::ZLIB ALIAS zlib)
+    target_include_directories(zlib INTERFACE ${zlib_SOURCE_DIR})
+    message(STATUS "found zlib")
+endif()
+
+# find_package() for minizip is broken on fedora
+# not sure other distros have .cmake for minizip either
+# so using the old fashioned find_() has to be used
+find_library(minizip_lib minizip)
+find_path(minizip_inc minizip)
+
+if (minizip_lib AND minizip_inc)
+    target_link_libraries(mgb PRIVATE ${minizip_lib})
+    target_include_directories(mgb PRIVATE ${minizip_inc})
+
+    # this has to be linked after minizip!
+    target_link_libraries(mgb PRIVATE ZLIB::ZLIB)
+
+    set(MINIZIP_FOUND TRUE)
+    message(STATUS "using system minizip")
+endif()
+
+# failed to find minizip installed, fetch and build manually
+if (NOT MINIZIP_FOUND)
+    message(STATUS "populating minizip")
+    # fetch the same version as zlib :)
+    set(MINIZIP_VERSION ${ZLIB_VERSION})
+    # work around bug where v1.3 is defined as v1.3.0
+    if (MINIZIP_VERSION EQUAL 1.3.0)
+        set(MINIZIP_VERSION 1.3)
+    endif()
+
+    FetchContent_Declare(minizip
+        GIT_REPOSITORY https://github.com/madler/zlib.git
+        GIT_TAG        v${MINIZIP_VERSION}
+        GIT_PROGRESS   TRUE
+        SOURCE_SUBDIR NONE
+    )
+
+    FetchContent_MakeAvailable(minizip)
+
+    add_library(minizip
+        ${minizip_SOURCE_DIR}/contrib/minizip/ioapi.c
+        ${minizip_SOURCE_DIR}/contrib/minizip/zip.c
+        ${minizip_SOURCE_DIR}/contrib/minizip/unzip.c
+    )
+
+    target_include_directories(minizip PRIVATE ${minizip_SOURCE_DIR}/contrib/minizip/)
+    # need the paths to be minizip/unzip.h
+    target_include_directories(minizip PUBLIC ${minizip_SOURCE_DIR}/contrib/)
+
+    target_link_libraries(minizip PRIVATE ZLIB::ZLIB)
+    target_link_libraries(mgb PRIVATE minizip)
+
+    message(STATUS "using github minizip")
+endif()
 
 target_link_libraries(mgb PRIVATE
     TotalSMS
-    # rewind
-    minizip::minizip
-    ZLIB::ZLIB
     patch
+    ZLIB::ZLIB
 )

src/mgb/compressors.c
@@ -1,74 +1,0 @@
-#if 0
-#include <zstd.h>
-#include <lz4.h>
-#endif
-#include <zlib.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stddef.h>
-
-
-enum CompressMode
-{
-    CompressMode_DEFLATE,
-    CompressMode_INFLATE,
-};
-
-size_t Zlib_size(size_t src_size)
-{
-    return compressBound(src_size);
-}
-
-size_t Zlib(void* dst_data, size_t dst_size, const void* src_data, size_t src_size, int mode)
-{
-    int result = 0;
-    uLongf dst_size_ = 0; // silence warn for ulong being different size to size_t
-
-    if (mode == CompressMode_DEFLATE)
-    {
-        result = compress(dst_data, &dst_size_, src_data, src_size);
-    }
-    else
-    {
-        result = uncompress(dst_data, &dst_size_, src_data, src_size);
-    }
-    dst_size = dst_size_;
-
-    return result == Z_OK ? dst_size : (size_t) - 1;
-}
-
-#if 0
-size_t Zstd_size(size_t src_size)
-{
-    return ZSTD_compressBound(src_size);
-}
-
-size_t Zstd(void* dst_data, size_t dst_size, const void* src_data, size_t src_size, int mode)
-{
-    if (mode == CompressMode_DEFLATE)
-    {
-        return ZSTD_compress(dst_data, dst_size, src_data, src_size, ZSTD_CLEVEL_DEFAULT);
-    }
-    else
-    {
-        return ZSTD_decompress(dst_data, dst_size, src_data, src_size);
-    }
-}
-
-size_t Lz4_size(size_t src_size)
-{
-    return LZ4_compressBound(src_size);
-}
-
-size_t Lz4(void* dst_data, size_t dst_size, const void* src_data, size_t src_size, int mode)
-{
-    if (mode == CompressMode_DEFLATE)
-    {
-        return LZ4_compress_default(src_data, dst_data, src_size, dst_size);
-    }
-    else
-    {
-        return LZ4_decompress_safe(src_data, dst_data, src_size, dst_size);
-    }
-}
-#endif

src/mgb/compressors.h
@@ -1,21 +1,0 @@
-#pragma once
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <stdint.h>
-#include <stddef.h>
-
-size_t Zlib_size(size_t src_size);
-size_t Zlib(void* dst_data, size_t dst_size, const void* src_data, size_t src_size, int mode);
-
-size_t Zstd_size(size_t src_size);
-size_t Zstd(void* dst_data, size_t dst_size, const void* src_data, size_t src_size, int mode);
-
-size_t Lz4_size(size_t src_size);
-size_t Lz4(void* dst_data, size_t dst_size, const void* src_data, size_t src_size, int mode);
-
-#ifdef __cplusplus
-}
-#endif

src/mgb/mgb.c
@@ -983,57 +983,3 @@ void mgb_set_on_convert_pixels_to_png_format(convert_pixels_to_png_format_func c
 {
     mgb.on_png_convert_cb = cb;
 }
-
-// commented out as state code is being replaced
-#if 0
-#include "rewind.h"
-#include "compressors.h"
-
-static struct RewindState rewind_state = {0};
-static struct Rewind rewinder = {0};
-
-bool mgb_rewind_init(size_t frames)
-{
-    rewind_init(&rewinder, Zlib, Zlib_size, frames);
-    // rewind_add_compressor(&rewinder, Zlib, Zlib_size);
-    // rewind_add_compressor(&rewinder, Zstd, Zstd_size);
-    // rewind_add_compressor(&rewinder, Lz4, Lz4_size);
-    return true;
-}
-
-void mgb_rewind_close(void)
-{
-    rewind_close(&rewinder);
-}
-
-// save states and stores pixel data for that frame
-bool mgb_rewind_push_frame(const void* pixels, size_t size)
-{
-    if (SMS_savestate(mgb.sms, &rewind_state.state))
-    {
-        memcpy(rewind_state.pixels, pixels, size);
-
-        if (rewind_push(&rewinder, (const uint8_t*)&rewind_state, sizeof(rewind_state)))
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-// loads state and loads pixel data for that frame
-bool mgb_rewind_pop_frame(void* pixels, size_t size)
-{
-    if (rewind_pop(&rewinder, (uint8_t*)&rewind_state, sizeof(rewind_state)))
-    {
-        if (SMS_loadstate(mgb.sms, &rewind_state.state))
-        {
-            memcpy(pixels, rewind_state.pixels, size);
-            return true;
-        }
-    }
-
-    return false;
-}
-#endif

src/third-party/minizip/CMakeLists.txt
@@ -1,18 +1,0 @@
-cmake_minimum_required(VERSION 3.13.4)
-
-project(minizip LANGUAGES C)
-
-add_library(minizip
-    ioapi_mem.c
-    ioapi.c
-    unzip.c
-    zip.c
-)
-add_library(minizip::minizip ALIAS ${PROJECT_NAME})
-
-target_include_directories(minizip INTERFACE ${CMAKE_CURRENT_SOURCE_DIR})
-target_compile_definitions(minizip PRIVATE NOCRYPT=1)
-target_compile_definitions(minizip PRIVATE NOUNCRYPT=1)
-
-find_shared_lib(ZLIB zlib)
-target_link_libraries(minizip LINK_PRIVATE ZLIB::ZLIB)

commit 1cd3916bdb4ca9eaa4a05201c9f878e0f7981c7c

fix zip mem loading bug introduced in 70dd13b, try web build caching.

.github/workflows/deploy_web.yml
@@ -1,5 +1,10 @@
 name: Build Web & Deploy to GH
 on: [push]
+
+env:
+  EM_VERSION: '4.0.7'
+  EM_CACHE_FOLDER: 'emsdk-cache'
+
 jobs:
   build-and-deploy:
     runs-on: ubuntu-latest
@@ -7,11 +12,18 @@ jobs:
       - name: Checkout
         uses: actions/checkout@v3
 
+      - name: Setup cache
+        id: cache-system-libraries
+        uses: actions/cache@v4
+        with:
+          path: ${{env.EM_CACHE_FOLDER}}
+          key: ${{env.EM_VERSION}}-${{ runner.os }}
+
       - name: Setup emsdk
         uses: mymindstorm/setup-emsdk@v14
         with:
-          version: '4.0.7'
-          actions-cache-folder: 'emsdk-cache'
+          version: ${{env.EM_VERSION}}
+          actions-cache-folder: ${{env.EM_CACHE_FOLDER}}
 
       - name: Install and Build # This example project is built using npm and outputs the result to the 'build' folder. Replace with the commands required to build your project, or remove this step entirely if your site is pre-built.
         run: |

src/mgb/ifile/zip/zip.c
@@ -573,7 +573,6 @@ static IFile_t* open_read_mem(union Data data, size_t size, enum IFileMode mode,
     unzFile file = NULL;
     MzMem* mzmem = NULL;
     zlib_filefunc_def filefunc32 = zlib_filefunc;
-    filefunc32.opaque =
 
     ifile = (IFile_t*)malloc(sizeof(IFile_t));
     if (!ifile) {
@@ -594,6 +593,7 @@ static IFile_t* open_read_mem(union Data data, size_t size, enum IFileMode mode,
     mzmem->size = size;
     mzmem->offset = 0;
     mzmem->read_only = mode == IFileMode_READ;
+    filefunc32.opaque = mzmem;
 
     file = unzOpen2("__notused__", &filefunc32);
     if (!file) {